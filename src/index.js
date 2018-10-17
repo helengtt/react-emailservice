766035e9d10ae55fffccdb0a124a4a426cccb8be
Helen Guo <helengreen2008@gmail.com>
2018-10-17 11:13:17 +1100

    doc: change name Main.js to App.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Main from "./Main";
+import Main from "./App";
  
 ReactDOM.render(
-  <Main/>, 
+  <App/>, 
   document.getElementById("root")
 );
